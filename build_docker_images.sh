175f7aafffa3855a04d73e5461e3270171ce797e
jer-nc <rejdev24@gmail.com>
2024-07-16 13:36:49 +0200

    chore: Refactor build_docker_images.sh script

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# TODO: Build doker images from array of Blender versions
-
 REPO_ECR_NAME=$1
 BLENDER_VERSIONS=$2
 AWS_ACCOUNT_ID=$3
@@ -19,16 +17,13 @@ get_major_version() {
 
 get_full_version() {
     version=$1
-    full_version="${version%.*}"  # Extraer la versión principal
+    full_version="${version%.*}"  # Extract major version from full version
     echo "$full_version"
 }
 
 image_exists_in_ecr() {
     major_version=$1
-    # echo "Checking if image exists in ECR for major_version: $major_version"
     image_exists=$(docker manifest inspect $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$REPO_ECR_NAME:$major_version 2>&1)
-
-    # echo "Image exists: $image_exists"
     
     if [[ $image_exists == *"no such manifest"* ]]; then
         return 1
